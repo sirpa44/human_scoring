7dc0980671196b19240f957512ef97100cc09de8
sirpa44 <sirpa.antoine@gmail.com>
2019-02-04 18:02:43 +0100

    authentication functional

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\ScorerEntity;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // you can fetch the EntityManager via $this->getDoctrine()
+        // or you can add an argument to your action: index(EntityManagerInterface $entityManager)
+        //$entityManager = $this->getDoctrine()->getManager();
+
+        //$scorerEntity = new ScorerEntity();
+        //$scorerEntity->setUsername('toto');
+        //$scorerEntity->setPassword('toto');
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        //$entityManager->persist($scorerEntity);
+
+        // actually executes the queries (i.e. the INSERT query)
+        //$entityManager->flush();
+
+        //return new Response('Saved new product with id '.$scorerEntity->getId());
+
+
+
+
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+}
