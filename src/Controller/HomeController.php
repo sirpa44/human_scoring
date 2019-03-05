5a0e87d66d0e5b5e28db6968bbfb4c76064d3c11
sirpa44 <sirpa.antoine@gmail.com>
2019-03-05 14:17:40 +0100

    add declare strict in each files

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types = 1);
 /**
  * Human Scoring Software
  *
@@ -12,7 +12,6 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Security\Core\Security;
 
 
 class HomeController extends AbstractController
--- tests/Unit/Entity/ScorerEntityTest.php
+++ tests/Unit/Entity/ScorerEntityTest.php
@@ -1,5 +1,10 @@
-<?php
-
+<?php declare(strict_types = 1);
+/**
+ * Human Scoring Software
+ *
+ * @author antoinep@taotesting.com
+ * @license See LICENCE.md
+ */
 namespace App\tests\Unit\Tests\Entity;
 
 use App\Entity\ScorerEntity;
